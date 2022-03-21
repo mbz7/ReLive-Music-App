88daba83bc59ee001b8040b0dfdc76e1053e994d
Myles <bracken.myles@gmail.com>
2022-03-20 20:51:00 -0700

    fixed authorization issues, updated concert serializer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
-  before_action :authorized
-  def authorized
-    unless session.include? :user_id
-      return render json: { error: 'Not Authorized' }, status: :unauthorized
-    end
-  end
+  # before_action :authorized
+  # def authorized
+  #   unless session.include? :user_id
+  #     return render json: { error: 'Not Authorized' }, status: :unauthorized
+  #   end
+  # end
 end
--- app/serializers/concert_serializer.rb
+++ app/serializers/concert_serializer.rb
@@ -1,4 +1,3 @@
 class ConcertSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :band, :venue, :location, :date
-  has_one :user
 end
